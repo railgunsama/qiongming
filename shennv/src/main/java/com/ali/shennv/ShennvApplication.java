2911328b8c9bb8964c8823a13c40b306f0ea8298
lxp <18245758687@163.com>
2021-03-14 22:35:31 +0800

    home first commit

---
+++ shennv/src/main/java/com/ali/shennv/ShennvApplication.java
@@ -0,0 +1,12 @@
+package com.ali.shennv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ShennvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShennvApplication.class, args);
+    }
+
+}
